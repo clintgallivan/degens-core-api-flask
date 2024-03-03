c80476aeb82741ced0ec3087ddbc8c75d45389f2
Clint Gallivan <clint@metalpay.co>
2024-03-03 13:29:17 -0800

    update substring search updater function and token list updater function

---
+++ src/functions/update_token_list.py
@@ -0,0 +1,37 @@
+
+# * Run this once per day before any jobs execute to update the token list
+# TODO: try and batch upate to mongodb because it posts one at a time and takes a long time
+import os
+import requests
+from pymongo import MongoClient
+
+cluster = MongoClient(os.getenv('MONGO_DB_SRV'), connect=False)
+db = cluster["tokens"]
+collection = db["token-list"]
+
+coingecko_base_url = os.getenv('COINGECKO_BASE_URL')
+headers = {
+    'x-cg-pro-api-key': os.getenv('COINGECKO_API_KEY'),
+}
+
+def get_token_list():
+    r = requests.get(f'{coingecko_base_url}/coins/list', headers=headers)
+    return r.json()
+
+def update_token_list_in_db(token_list):
+    for token in token_list:
+        # Map the token to the expected structure
+        mapped_token = {
+            'coingecko_id': token['id'],
+        }
+        collection.update_one(
+            {'coingecko_id': token['id']},  # Query
+            {'$set': mapped_token},  # Update
+            upsert=True  # If the document doesn't exist, create it
+        )
+        print(f"updated-token: {token['id']}")
+
+def get_token_list_and_update_db_concurrently():
+    update_token_list_in_db(get_token_list())
+
+get_token_list_and_update_db_concurrently()
--- src/schedulers/functions/get_substr_search_data.py
+++ src/schedulers/functions/get_substr_search_data.py
@@ -1,41 +1,41 @@
 
-# * Run once a day to update token timestamp - call get_token_timestamp_and_post_concurrently(token_id_list)
+# * Run once a day to update the substr-search collection - call get_token_substr_data_and_post_concurrently()
 import os
-from operator import index
 import requests
-import pandas as pd
-import numpy as np
-import time
-import random
 import json
+from pymongo import MongoClient
 
-mongo_base_url = os.getenv(host=os.getenv('MONGO_DB_SRV'), connect=False)
+# Create a MongoDB client
+cluster = MongoClient(os.getenv('MONGO_DB_SRV'), connect=False)
+
+# Get the 'tokens' database and the 'token-metadata' collection
+db = cluster["tokens"]
+collection_token_metadata = db["token-metadata"]
+collection_substr_search = db["substr-search"]
 
 
 def get_token_current_metadata():
-    r = requests.get(f'{mongo_base_url}/tokens', timeout=600)
-    return r.json()
-
+    # Fetch data directly from MongoDB collection
+    tokens = collection_token_metadata.find()
+    return [token for token in tokens]
 
 def post_to_substring_db(ds):
-    headers = {'Content-type': 'application/json'}
-    r = requests.post(f'{mongo_base_url}/substr-tokens',
-                      data=ds, headers=headers)
-    print(r)
-
+    collection_substr_search.update_one(
+        {'coingecko_id': ds['coingecko_id']},  # Query
+        {'$set': ds},  # Update
+        upsert=True  # If the document doesn't exist, create it
+    )
+    print(f"updated-token: {ds['coingecko_id']}")
 
 def reduce_data_and_post(table):
     for item in table:
         ds = {}
         ds["coingecko_id"] = item.get('coingecko_id', '')
-        # ds["symbol"] = item.get('symbol', '')
-        # ds["name"] = item.get("name", '')
         ds["image"] = item.get("image", '')
         ds["market_cap_rank"] = item.get("market_cap_rank", '')
         ds["degen_rank"] = item.get("degen_rank", '')
         ds["iterator"] = [item.get("name", ''), item.get("symbol", '')]
-        post_to_substring_db(json.dumps(ds))
-
+        post_to_substring_db(ds)
 
 def get_token_substr_data_and_post_concurrently():
     print('beginning parse through token list')
@@ -43,57 +43,4 @@ def get_token_substr_data_and_post_concurrently():
     reduce_data_and_post(table)
     print('done')
 
-
 get_token_substr_data_and_post_concurrently()
-
-
-# * Run once a day to insert top token document - call get_token_timestamp_and_post_concurrently(token_id_list)
-
-# # * Run once a day to update token timestamp - call get_token_timestamp_and_post_concurrently(token_id_list)
-# import os
-# from operator import index
-# import requests
-# import pandas as pd
-# import numpy as np
-# import time
-# import random
-# import json
-
-mongo_base_url = os.getenv('MONGO_DB_BASE_URL')
-
-
-# def get_token_current_metadata():
-#     r = requests.get(f'{mongo_base_url}/tokens', timeout=600)
-#     return r.json()
-
-
-# def post_to_substring_db(ds):
-#     headers = {'Content-type': 'application/json'}
-#     r = requests.post(f'{mongo_base_url}/substr-tokens',
-#                       data=ds, headers=headers)
-#     print(r)
-
-
-# def reduce_data_and_post(table):
-#     for item in table:
-#         ds = {}
-#         ds["coingecko_id"] = item.get('coingecko_id', '')
-#         ds["symbol"] = item.get('symbol', '')
-#         ds["name"] = item.get("name", '')
-#         ds["image"] = item.get("image", '')
-#         ds["market_cap_rank"] = item.get("market_cap_rank", '')
-#         ds["degen_rank"] = item.get("degen_rank", '')
-#         post_to_substring_db(json.dumps(ds))
-
-
-# def get_token_substr_data_and_post_concurrently():
-#     print('beginning parse through token list')
-#     table = get_token_current_metadata()
-#     reduce_data_and_post(table)
-#     print('done')
-
-
-# get_token_substr_data_and_post_concurrently()
-
-
-# # * Run once a day to insert top token document - call get_token_timestamp_and_post_concurrently(token_id_list)
