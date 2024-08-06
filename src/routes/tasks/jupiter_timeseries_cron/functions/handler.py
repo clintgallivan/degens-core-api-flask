48e029b745a068a0bcfa41e411c9ed4b41b32a95
Clint Gallivan <clint@metalpay.co>
2024-08-05 19:33:29 -0700

    feat: attempt at scaling jupiter_timeseries_cron

--- src/routes/tasks/jupiter_timeseries_cron/functions/handler.py
+++ src/routes/tasks/jupiter_timeseries_cron/functions/handler.py
@@ -1,16 +1,43 @@
 import time
+import json
+import os
 from .get_jupiter_data import get_jupiter_data
 from .update_tokens_timeseries import update_tokens_timeseries
+from datetime import datetime
 
 LOG_TAG = ['jupiter_timeseries_cron.handler']
 
-def handler():
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return obj.isoformat()
+        return super().default(obj)
+
+# * [FOR LOCAL DEBUG] 1. Set use_local_data to True to use local data file in root of repo called "example_jupiter_data.json"
+def handler(use_local_data=False, local_data_file='example_jupiter_data.json'):
     start_time = time.time()
     
-    jupiter_data = get_jupiter_data()
-    print(f"{LOG_TAG} Fetching jupiter data took: {time.time() - start_time:.2f} seconds")
-    update_tokens_timeseries(jupiter_data)
+    if use_local_data and os.path.exists(local_data_file):
+        with open(local_data_file, 'r') as f:
+            jupiter_data = json.load(f)
+        
+        # Convert created_at fields back to datetime objects
+        for item in jupiter_data:
+            if 'created_at' in item:
+                item['created_at'] = datetime.fromisoformat(item['created_at'])
+        
+        print(f"{LOG_TAG} Loaded jupiter data from {local_data_file}")
+    else:
+        jupiter_data = get_jupiter_data()
+        print(f"{LOG_TAG} Fetching jupiter data took: {time.time() - start_time:.2f} seconds")
+    
+    update_tokens_timeseries(jupiter_data, start_time)
     
     print(f"{LOG_TAG} Handler execution time: {time.time() - start_time:.2f} seconds")
     
     return
+
+def save_jupiter_data_to_file(data, filename='example_jupiter_data.json'):
+    with open(filename, 'w') as f:
+        json.dump(data, f, cls=DateTimeEncoder)
+    print(f"Data saved to {filename}")
--- src/routes/tasks/jupiter_timeseries_cron/functions/update_tokens_timeseries.py
+++ src/routes/tasks/jupiter_timeseries_cron/functions/update_tokens_timeseries.py
@@ -8,16 +8,18 @@ cluster = MongoClient(host=os.getenv('MONGO_DB_SRV'), connect=True)  # Utilizing
 db = cluster['prod']
 timeseries_collection = db['tokens-timeseries-5m-interval-28h-expiry']
 
-def insert_documents(documents):
+LOG_TAG = ['jupiter_timeseries_cron.update_tokens_timeseries']
+def insert_documents(documents, start_time):
     if documents:
         timeseries_collection.insert_many(documents, ordered=False)
+        print(f"{LOG_TAG} Batch inserted after: {time.time() - start_time:.2f} seconds")
 
-def update_tokens_timeseries(jupiter_data):
+def update_tokens_timeseries(jupiter_data, start_time):
     documents = []
-    batch_size = 500
+    batch_size = 30000
     futures = []
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
         for data in jupiter_data:
             mint_address = data['mint_address']
             created_at = data['created_at']
@@ -31,12 +33,12 @@ def update_tokens_timeseries(jupiter_data):
             documents.append(document)
 
             if len(documents) == batch_size:
-                futures.append(executor.submit(insert_documents, documents))
+                futures.append(executor.submit(insert_documents, documents, start_time))
                 documents = []
 
         # Execute any remaining operations in the batch
         if documents:
-            futures.append(executor.submit(insert_documents, documents))
+            futures.append(executor.submit(insert_documents, documents, start_time))
 
         # Wait for all futures to complete
         concurrent.futures.wait(futures)
